eb6c514b3d0d5c8562e9800748a601734ea16fc4
Rafael Camargo <contato@rafaelcamargo.com>
2015-07-31 13:03:35 -0300

    Fixed karma conf and added the first test

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'dist/**/*.js': 'coverage'
+        'src/**/*.js': 'coverage'
     },
 
 
